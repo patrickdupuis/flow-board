f9330ca58c8131c5c142e1082483811ad604ccd0
Patrick Dupuis <pdupuis@tutanota.com>
2022-04-08 22:25:35 -0400

    seperate datable connection to own file

--- server/index.js
+++ server/index.js
@@ -1,33 +1,25 @@
+require("dotenv").config({ path: "../.env" });
+
 const express = require("express");
 const cors = require("cors");
+const PORT = process.env.PORT || 4000;
+const db = require("./db/connection");
 const authRoutes = require("./routes/authentication");
-const mongoose = require("mongoose");
-require("dotenv").config({ path: "../.env" });
-const { MONGO_URI } = process.env;
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
-const port = 4000;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose
-  .connect(MONGO_URI, options)
-  .then(() => console.log("Connected to DB"))
-  .catch((err) => {
-    console.error(err);
-    process.exit();
-  });
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-
 app.use("/auth", authRoutes);
 
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+db.connectToServer((err) => {
+  if (err) {
+    console.error(err);
+    process.exit();
+  }
+
+  app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+  });
 });
