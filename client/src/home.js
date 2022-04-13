2fd3020a0058809da1a4a661396be0d826bb65ac
Patrick Dupuis <pdupuis@tutanota.com>
2022-04-12 20:09:39 -0400

    migrate all CSS to styled-components

--- client/src/home.js
+++ client/src/home.js
@@ -1,6 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import styled from "styled-components";
 import logo from "./logo.svg";
-import "./home.css";
 import LoginButton from "./login";
 import LogoutButton from "./logout";
 import Profile from "./profile";
@@ -9,14 +9,47 @@ function Home() {
   const { isAuthenticated } = useAuth0();
 
   return (
-    <div className="Home">
-      <header className="Home-header">
-        <img src={logo} className="Home-logo" alt="logo" />
+    <Wrapper>
+      <Header>
+        <Logo src={logo} alt="logo" />
         <Profile />
         {!isAuthenticated ? <LoginButton /> : <LogoutButton />}
-      </header>
-    </div>
+      </Header>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  text-align: center;
+`;
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const Logo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+
+  @keyframes Home-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+  @media (prefers-reduced-motion: no-preference) {
+    animation: Home-logo-spin infinite 20s linear;
+  }
+`;
 
 export default Home;
--- client/src/index.js
+++ client/src/index.js
@@ -6,8 +6,6 @@ import App from "./app";
 import AuthorizationProvider from "./authorization-provider";
 import GlobalStyle from "./GlobalStyle";
 
-import "./index.css";
-
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
