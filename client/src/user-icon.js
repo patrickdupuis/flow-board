e60f061bd5cce1e6ed0634723c116c5635f61293
Patrick Dupuis <pdupuis@tutanota.com>
2022-04-14 17:27:33 -0400

    add user icon to header when authenticated

---
+++ client/src/user-icon.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import { FaUserCircle, FaChevronDown } from "react-icons/fa";
+
+const UserIcon = () => {
+  return (
+    <Wrapper>
+      <FaUserCircle size={25} />
+      <FaChevronDown />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  cursor: pointer;
+`;
+
+export default UserIcon;
