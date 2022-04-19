1831221dd456d11fcd2218b5d64c12b2b6ce8ae8
Patrick Dupuis <pdupuis@tutanota.com>
2022-04-19 17:23:50 -0400

    add BoardContext

--- client/src/project.js
+++ client/src/project.js
@@ -1,11 +1,12 @@
+import BoardProvider from "./board-context";
 import TaskBoard from "./task-board";
 
 const Project = () => {
   return (
-    <>
+    <BoardProvider>
       <h3>My Project Board</h3>
       <TaskBoard />
-    </>
+    </BoardProvider>
   );
 };
 
--- client/src/task-board.js
+++ client/src/task-board.js
@@ -1,78 +1,14 @@
-import { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+import { useContext } from "react";
 import { DragDropContext } from "@react-forked/dnd";
 import styled from "styled-components";
+import { BoardContext } from "./board-context";
 import TaskList from "./task-list";
 import TaskSearch from "./task-search";
 import Card from "./card";
 
-const listTitles = ["Search", "To Do", "In Progress", "Done"];
-
-// helper function for creating fake tasks
-// const getItems = (count, offset = 0) => {
-//   return Array.from({ length: count }, (v, k) => k).map((k) => ({
-//     id: uuidv4(),
-//     content: `item ${k + offset}`,
-//   }));
-// };
-
-const reorder = (list, startIndex, endIndex) => {
-  const result = Array.from(list);
-  const [removed] = result.splice(startIndex, 1);
-  result.splice(endIndex, 0, removed);
-
-  return result;
-};
-
-const move = (source, destination, droppableSource, droppableDestination) => {
-  const sourceClone = Array.from(source);
-  const destClone = Array.from(destination);
-  const [removed] = sourceClone.splice(droppableSource.index, 1);
-
-  destClone.splice(droppableDestination.index, 0, removed);
-
-  const result = {};
-  result[droppableSource.droppableId] = sourceClone;
-  result[droppableDestination.droppableId] = destClone;
-
-  return result;
-};
-
 const TaskBoard = () => {
-  const [state, setState] = useState(
-    Array.from({ length: listTitles.length }).map(() => [])
-  );
-
-  const setSearchResults = (searchResults) => {
-    const newState = Array.from(state);
-    newState.shift();
-    newState.unshift(searchResults);
-    setState(newState);
-  };
-
-  const onDragEnd = (result) => {
-    const { source, destination } = result;
-
-    // dropped outside the list
-    if (!destination) {
-      return;
-    }
-    const sInd = Number(source.droppableId);
-    const dInd = Number(destination.droppableId);
-
-    if (sInd === dInd) {
-      const items = reorder(state[sInd], source.index, destination.index);
-      const newState = [...state];
-      newState[sInd] = items;
-      setState(newState);
-    } else {
-      const result = move(state[sInd], state[dInd], source, destination);
-      const newState = [...state];
-      newState[sInd] = result[sInd];
-      newState[dInd] = result[dInd];
-      setState(newState);
-    }
-  };
+  const { listTitles, onDragEnd, state, setSearchResults } =
+    useContext(BoardContext);
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
